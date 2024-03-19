c44f72c19828ca4ac3789941caca114a095aed46
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-03-19 09:28:55 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Coroutine called async_generator that takes no arguments"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """The coroutine will loop 10 times
+    each time asynchronously wait 1 second,
+    then yield a random number between
+    0 and 10. Use the random module"""
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,4 @@
+0x02. Python - Async Comprehension
+This project mainly covers these two concepts:
+1. Python
+2. Back-end
