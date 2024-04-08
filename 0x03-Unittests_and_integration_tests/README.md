45960213e59abb2990b7c5c11b0a7944c769d411
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-08 03:25:10 +0000

    Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,5 @@
+0x03. Unittests and Integration Tests
+This project covers concepts such as:
+1. UnitTests
+2. Back-end
+3. Integration tests
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module containing unit tests for utils.access_nested_map function.
+"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test case for access_nested_map function.
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Test access_nested_map function for different inputs.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
