bece14eba147be2bd2750aebad35043bec880f6c
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-09 02:45:40 +0000

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 Unit tests for client.GithubOrgClient
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from client import GithubOrgClient
 
 
@@ -12,25 +12,36 @@ class TestGithubOrgClient(unittest.TestCase):
     Class to test GithubOrgClient class
     """
 
-    def test_public_repos_url(self):
+    def test_public_repos(self):
         """
-        Test GithubOrgClient._public_repos_url method
+        Test GithubOrgClient.public_repos method
         """
         # Define a known payload
-        payload = {"repos_url": "https://api.github.com/orgs/test/repos"}
+        payload = [{"name": "repo1"}, {"name": "repo2"}]
 
-        # Patch GithubOrgClient.org to return known payload
-        with patch('client.GithubOrgClient.org', return_value=payload):
-            # Create instance of GithubOrgClient
-            client = GithubOrgClient('test')
+        # Mocking get_json
+        with patch(
+                'client.GithubOrgClient.get_json', return_value=payload
+        ) as mock_get_json:
+            # Mocking _public_repos_url
+            with patch(
+                    'client.GithubOrgClient._public_repos_url',
+                    return_value="https://example.com"
+            ) as mock_public_repos_url:
+                # Create instance of GithubOrgClient
+                client = GithubOrgClient('test')
 
-            # Retrieve public repos URL
-            public_repos_url = client._public_repos_url
+                # Call public_repos method
+                repos = client.public_repos()
 
-            # Assert that the public repos URL is the expected one
-            self.assertEqual(
-                    public_repos_url, "https://api.github.com/orgs/test/repos"
-            )
+                # Assert that get_json was called once
+                mock_get_json.assert_called_once()
+
+                # Assert that _public_repos_url was called once
+                mock_public_repos_url.assert_called_once()
+
+                # Assert the result is as expected
+                self.assertEqual(repos, payload)
 
 
 if __name__ == '__main__':
