bbda32cc45209732293444ac03f49269ff695077
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-09 02:11:16 +0000

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,45 +1,37 @@
 #!/usr/bin/env python3
 """
-Unit tests for utils.memoize
+Unit tests for client.GithubOrgClient
 """
 import unittest
 from unittest.mock import patch
-from utils import memoize
+from parameterized import parameterized
+from client import GithubOrgClient
 
 
-class TestMemoize(unittest.TestCase):
+class TestGithubOrgClient(unittest.TestCase):
     """
-    Class to test memoize decorator
+    Class to test GithubOrgClient class
     """
 
-    class TestClass:
+    @parameterized.expand([
+        ("google",),
+        ("abc",)
+    ])
+    @patch('client.GithubOrgClient.get_json')
+    def test_org(self, org_name, mock_get_json):
         """
-        Class with a_method and memoized a_property
+        Test GithubOrgClient.org method
         """
-        def a_method(self):
-            return 42
+        # Instantiate GithubOrgClient with org_name
+        client = GithubOrgClient(org_name)
 
-        @memoize
-        def a_property(self):
-            return self.a_method()
+        # Call org method
+        client.org()
 
-    @patch('utils.TestMemoize.TestClass.a_method')
-    def test_memoize(self, mock_a_method):
-        """
-        Test memoize decorator
-        """
-        instance = self.TestClass()
-
-        # Call a_property twice
-        result1 = instance.a_property()
-        result2 = instance.a_property()
-
-        # Assert that a_method was called only once
-        mock_a_method.assert_called_once()
-
-        # Assert that the results are correct
-        self.assertEqual(result1, 42)
-        self.assertEqual(result2, 42)
+        # Assert that get_json was called once with the correct argument
+        mock_get_json.assert_called_once_with(
+                f'https://api.github.com/orgs/{org_name}'
+                )
 
 
 if __name__ == '__main__':
