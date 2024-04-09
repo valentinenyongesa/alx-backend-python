23e2e19cbc751c0d44354574f118c15407dde41d
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-09 02:55:02 +0000

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,35 +1,54 @@
 #!/usr/bin/env python3
 
 """
-Unit tests for client.GithubOrgClient
+Integration tests for GithubOrgClient class
 """
 
 import unittest
-from parameterized import parameterized
+from unittest.mock import patch
+from parameterized import parameterized_class
 from client import GithubOrgClient
+from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
-class TestGithubOrgClient(unittest.TestCase):
+@parameterized_class(('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+                     [(org_payload(), repos_payload(), expected_repos(), apache2_repos())])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
     """
-    Class to test GithubOrgClient class
+    Integration tests for GithubOrgClient class
     """
 
-    @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False)
-    ])
-    def test_has_license(self, repo, license_key, expected_result):
+    @classmethod
+    def setUpClass(cls):
         """
-        Test GithubOrgClient.has_license method
+        Set up the test class
+        """
+        cls.get_patcher = patch('client.requests.get')
+
+        # Start patcher and mock requests.get with side_effect
+        cls.mock_get = cls.get_patcher.start()
+        cls.mock_get.side_effect = [cls.org_payload, cls.repos_payload]
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Tear down the test class
+        """
+        # Stop patcher
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """
+        Test public_repos method
         """
         # Create instance of GithubOrgClient
         client = GithubOrgClient('test')
 
-        # Call has_license method
-        result = client.has_license(repo, license_key)
+        # Call public_repos method
+        repos = client.public_repos()
 
         # Assert that the result is as expected
-        self.assertEqual(result, expected_result)
+        self.assertEqual(repos, self.expected_repos)
 
 
 if __name__ == '__main__':
