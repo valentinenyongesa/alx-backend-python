a16162dbfc13a66cf7e553fe32b7840bfdbaba01
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-09 02:50:08 +0000

    Parametrize the test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+
 """
 Unit tests for client.GithubOrgClient
 """
+
 import unittest
-from unittest.mock import patch, MagicMock
+from parameterized import parameterized
 from client import GithubOrgClient
 
 
@@ -12,36 +14,22 @@ class TestGithubOrgClient(unittest.TestCase):
     Class to test GithubOrgClient class
     """
 
-    def test_public_repos(self):
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
         """
-        Test GithubOrgClient.public_repos method
+        Test GithubOrgClient.has_license method
         """
-        # Define a known payload
-        payload = [{"name": "repo1"}, {"name": "repo2"}]
+        # Create instance of GithubOrgClient
+        client = GithubOrgClient('test')
 
-        # Mocking get_json
-        with patch(
-                'client.GithubOrgClient.get_json', return_value=payload
-        ) as mock_get_json:
-            # Mocking _public_repos_url
-            with patch(
-                    'client.GithubOrgClient._public_repos_url',
-                    return_value="https://example.com"
-            ) as mock_public_repos_url:
-                # Create instance of GithubOrgClient
-                client = GithubOrgClient('test')
+        # Call has_license method
+        result = client.has_license(repo, license_key)
 
-                # Call public_repos method
-                repos = client.public_repos()
-
-                # Assert that get_json was called once
-                mock_get_json.assert_called_once()
-
-                # Assert that _public_repos_url was called once
-                mock_public_repos_url.assert_called_once()
-
-                # Assert the result is as expected
-                self.assertEqual(repos, payload)
+        # Assert that the result is as expected
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
